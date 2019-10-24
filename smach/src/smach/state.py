9d07a60e6c29913a952faf7ba1ef1b8b8580699b
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2019-10-25 00:21:19 +0200

    Fix StateCodeTransformer

--- smach/src/smach/state.py
+++ smach/src/smach/state.py
@@ -68,6 +68,28 @@ class StateCodeTransformer(ast.NodeTransformer):
     def visit_Name(self, node):
         if node.id == "self":
             return ast.copy_location(ast.Attribute(attr="_state", value=node, ctx=ast.Load()), node)
+        else:
+            for field, old_value in ast.iter_fields(node):
+                old_value = getattr(node, field, None)
+                if isinstance(old_value, list):
+                    new_values = []
+                    for value in old_value:
+                        if isinstance(value, ast.AST):
+                            value = self.visit(value)
+                            if value is None:
+                                continue
+                            elif not isinstance(value, ast.AST):
+                                new_values.extend(value)
+                                continue
+                        new_values.append(value)
+                    old_value[:] = new_values
+                elif isinstance(old_value, ast.AST):
+                    new_node = self.visit(old_value)
+                    if new_node is None:
+                        delattr(node, field)
+                    else:
+                        setattr(node, field, new_node)
+            return node
 
 
 class StateCodeReverseTransformer(ast.NodeTransformer):
