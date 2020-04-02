02d35e2fd4cbb625e6ca6373687bf4c66609083c
ahcorde <ahcorde@gmail.com>
2020-04-02 16:49:17 +0200

    changes to make it work with Python3
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- smach_ros/src/smach_ros/__init__.py
+++ smach_ros/src/smach_ros/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2010, Willow Garage, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
 #     * Neither the name of the Willow Garage, Inc. nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -51,14 +51,14 @@ smach.set_shutdown_check(rospy.is_shutdown)
 smach.set_shutdown_handler(rospy.on_shutdown)
 
 ### Core classes
-from util import set_preempt_handler, start
+from smach_ros.util import set_preempt_handler, start
 
 ### Top-level Containers / Wrappers
-from action_server_wrapper import ActionServerWrapper
-from introspection import IntrospectionClient, IntrospectionServer
+from smach_ros.action_server_wrapper import ActionServerWrapper
+from smach_ros.introspection import IntrospectionClient, IntrospectionServer
 
 ### State Classes
-from simple_action_state import SimpleActionState
-from service_state import ServiceState
-from monitor_state import MonitorState
-from condition_state import ConditionState
+from smach_ros.simple_action_state import SimpleActionState
+from smach_ros.service_state import ServiceState
+from smach_ros.monitor_state import MonitorState
+from smach_ros.condition_state import ConditionState
